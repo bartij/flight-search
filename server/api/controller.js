85c7a3a5c68eabb742553dcd58ef179dd461dff2
Bartosz Jurga <barti.jurga@gmail.com>
2017-06-06 00:30:35 +0200

    promise based package for requests used

--- server/api/controller.js
+++ server/api/controller.js
@@ -1,4 +1,4 @@
-const http = require('http');
+const request = require('request-promise');
 
 const requestError = (status, content) => {
     const successCode = 200;
@@ -10,53 +10,70 @@ const requestError = (status, content) => {
         return null;
     }
 };
-const getAirLinesData = (host, path, res) => {
-    http.get({
-        hostname: host,
-        port: 80,
-        path: path,
-        agent: false
-    }, (response) => {
-        const { statusCode } = response;
-        const contentType = response.headers['content-type'];
-        const error = requestError(statusCode, contentType);
-        if (error) {
-            console.error(error.message);
-            response.resume();
-            return;
-        }
+// const getAirLinesData = (host, path, res) => {
+//     http.get({
+//         hostname: host,
+//         port: 80,
+//         path: path,
+//         agent: false
+//     }, (response) => {
+//         const { statusCode } = response;
+//         const contentType = response.headers['content-type'];
+//         const error = requestError(statusCode, contentType);
+//         if (error) {
+//             console.error(error.message);
+//             response.resume();
+//             return;
+//         }
+//
+//         response.setEncoding('utf8');
+//         let rawData = '';
+//         response.on('data', (chunk) => {
+//             rawData += chunk;
+//         });
+//         response.on('end', () => {
+//             try {
+//                 const parsedData = JSON.parse(rawData);
+//                 res.send(parsedData);
+//             } catch (e) {
+//                 console.error(e.message);
+//             }
+//         });
+//     }).on('error', (e) => {
+//         console.error(`Got error: ${e.message}`);
+//         return {
+//             statusCode: 500,
+//             statusMessage: e.message
+//         };
+//     });
+// };
 
-        response.setEncoding('utf8');
-        let rawData = '';
-        response.on('data', (chunk) => {
-            rawData += chunk;
-        });
-        response.on('end', () => {
-            try {
-                const parsedData = JSON.parse(rawData);
-                res.send(parsedData);
-            } catch (e) {
-                console.error(e.message);
-            }
-        });
-    }).on('error', (e) => {
+const getAirLinesData = (uri, res) => {
+    request({
+        uri: uri,
+        headers: {
+            'User-Agent': 'Request-Promise'
+        },
+        json: true
+    }).then((response) => {
+        try {
+            res.send(response);
+        } catch (e) {
+            console.error(e.message);
+        }
+    }).catch((e) => {
         console.error(`Got error: ${e.message}`);
-        return {
-            statusCode: 500,
-            statusMessage: e.message
-        };
     });
 };
 
-const locomoteHost = 'node.locomote.com';
 exports.airlines = function(req, res) {
-    getAirLinesData(locomoteHost, '/code-task/airlines', res);
+    getAirLinesData('http://node.locomote.com/code-task/airlines', res);
 };
 exports.airports = function(req, res) {
     const city = req.params.city;
-    getAirLinesData(locomoteHost, '/code-task/airports?q=' + city, res);
+    getAirLinesData('http://node.locomote.com/code-task/airports?q=' + city, res);
 };
 exports.search = function(req, res) {
     const { from, to, date } = req.params;
-    getAirLinesData(locomoteHost, '/code-task/flight_search/QF?date=2018-09-02&from=SYD&to=JFK', res);
+    getAirLinesData('http://node.locomote.com/code-task/flight_search/QF?date=2018-09-02&from=SYD&to=JFK', res);
 };
