805aaee1c87bf7eea096eb2d757d62c9b27cffe4
Bartosz Jurga <barti.jurga@gmail.com>
2017-06-06 20:52:30 +0200

    first flights fetched and moment.js used

--- server/api/controller.js
+++ server/api/controller.js
@@ -1,79 +1,73 @@
 const request = require('request-promise');
+const moment = require('moment');
 
-const requestError = (status, content) => {
-    const successCode = 200;
-    if (status !== successCode) {
-        return new Error('Request Failed.\n' + `Status Code: ${status}`);
-    } else if (!/^application\/json/.test(content)) {
-        return new Error('Invalid content-type.\n' + `Expected application/json but received ${content}`);
-    } else {
-        return null;
-    }
-};
-// const getAirLinesData = (host, path, res) => {
-//     http.get({
-//         hostname: host,
-//         port: 80,
-//         path: path,
-//         agent: false
-//     }, (response) => {
-//         const { statusCode } = response;
-//         const contentType = response.headers['content-type'];
-//         const error = requestError(statusCode, contentType);
-//         if (error) {
-//             console.error(error.message);
-//             response.resume();
-//             return;
-//         }
-//
-//         response.setEncoding('utf8');
-//         let rawData = '';
-//         response.on('data', (chunk) => {
-//             rawData += chunk;
-//         });
-//         response.on('end', () => {
-//             try {
-//                 const parsedData = JSON.parse(rawData);
-//                 res.send(parsedData);
-//             } catch (e) {
-//                 console.error(e.message);
-//             }
-//         });
-//     }).on('error', (e) => {
-//         console.error(`Got error: ${e.message}`);
-//         return {
-//             statusCode: 500,
-//             statusMessage: e.message
-//         };
-//     });
-// };
+const getApiData = (uri) => request({
+    uri: uri,
+    headers: {
+        'User-Agent': 'Request-Promise'
+    },
+    json: true
+});
 
-const getAirLinesData = (uri, res) => {
-    request({
-        uri: uri,
-        headers: {
-            'User-Agent': 'Request-Promise'
-        },
-        json: true
-    }).then((response) => {
-        try {
-            res.send(response);
-        } catch (e) {
-            console.error(e.message);
-        }
-    }).catch((e) => {
-        console.error(`Got error: ${e.message}`);
-    });
-};
+const datesArray = (middleDate) =>
+    [moment(middleDate).add(-2, 'd'), moment(middleDate).add(-1, 'd'), moment(middleDate),
+        moment(middleDate).add(1, 'd'), moment(middleDate).add(2, 'd')];
+
+const dateString = (date) => moment(date).format('YYYY-MM-DD');
+
+const createFlightSearchUrl = (airlineCode, date, from, to) =>
+    'http://node.locomote.com/code-task/flight_search/' + airlineCode + '?date=' + date + '&from=' + from + '&to=' + to;
+const airlinesUrl = 'http://node.locomote.com/code-task/airlines';
+const airportsUrl = 'http://node.locomote.com/code-task/airports?q=';
+const searchUrl = createFlightSearchUrl('QF','2018-09-02','SYD','JFK');
 
 exports.airlines = function(req, res) {
-    getAirLinesData('http://node.locomote.com/code-task/airlines', res);
+    getApiData(airlinesUrl)
+        .then((airlines) => {
+            try {
+                res.send(airlines);
+            } catch (e) {
+                console.error(e.message);
+            }
+        }).catch((e) => console.error(`Got error: ${e.message}`));
 };
 exports.airports = function(req, res) {
     const city = req.params.city;
-    getAirLinesData('http://node.locomote.com/code-task/airports?q=' + city, res);
+    getApiData(airportsUrl + city)
+        .then((airlines) => {
+            try {
+                res.send(airlines);
+            } catch (e) {
+                console.error(e.message);
+            }
+        }).catch((e) => console.error(`Got error: ${e.message}`));
 };
 exports.search = function(req, res) {
-    const { from, to, date } = req.params;
-    getAirLinesData('http://node.locomote.com/code-task/flight_search/QF?date=2018-09-02&from=SYD&to=JFK', res);
+    // const { from, to, date } = req.params;
+    const from = 'Sydney';
+    const to = 'New York';
+    const date = new Date(2017,10,25);
+    const getStartingLocationAirports = getApiData(airportsUrl + from);
+    const getDestinationAirports = getApiData(airportsUrl + to);
+    const getAirlines = getApiData(airlinesUrl);
+    const dates = datesArray(date);
+    Promise.all([getStartingLocationAirports, getDestinationAirports, getAirlines])
+        .then(fetchedData => {
+            const startLocationAirports = fetchedData[0];
+            const destinationAirports = fetchedData[1];
+            const airlines = fetchedData[2];
+            console.log('startLocationAirports[0]',startLocationAirports[0].airportCode);
+            const uri = createFlightSearchUrl(
+                airlines[0].code,
+                dateString(dates[0]),
+                startLocationAirports[0].airportCode,
+                destinationAirports[0].airportCode
+            );
+            console.log('url: ', uri);
+            console.log('url', searchUrl);
+            getApiData(uri)
+                .then(flight => console.log('\n\nflight:', flight))
+                .catch((e) => console.error(`Error while fetching flight: ${e.message}`));
+        })
+        .catch(error => console.error(`Fetching data error: ${error.message}`));
 };
